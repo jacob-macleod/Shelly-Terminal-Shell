a92aa57d6163a40d7fe9cd6847b3deb3dfc9c355
Jacob MacLeod <jacob.e.macleod@gmail.com>
2020-07-05 20:49:26 +0000

    Developed part of default core plugin

--- core_placeholder.py
+++ core_placeholder.py
@@ -1,6 +1,20 @@
-def core () :
-    return ">>> "
-    sys.stdout.write(u"\u001b[1000D") # Move all the way left again
-    sys.stdout.write(u"\u001b[1000D") # Move all the way left again
-    sys.stdout.write(u"\u001b[1000D") # Move all the way left again
-    sys.stdout.write(u"\u001b[1000D") # Move all the way left again
+import os
+
+def split(word): 
+    return [char for char in word] 
+
+def core (current_dir) :
+    str_username = ""
+    username = split(os.popen("whoami").read())
+    username.pop()
+
+    for i in range (0, len(username)) :
+        str_username += username[i]
+    current_dir_split = current_dir.split()
+    del current_dir_split[0]
+    if len(current_dir.split("/")) == 3:
+        
+        return str_username + "$ "
+    return str_username + "@" + current_dir + "$ "
+
+#Todo: remove first slash of the current directory, add syntax highlihhting, git support
