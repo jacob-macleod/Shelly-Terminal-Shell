ce09f428e7c63ef922f65c21c2762a4130274fd1
Jacob MacLeod <jacob.e.macleod@gmail.com>
2020-07-05 14:36:34 +0000

    Updated the defult core plugin - NOT READY YET!

--- core_placeholder.py
+++ core_placeholder.py
@@ -1,6 +1,9 @@
-def core () :
-    return ">>> "
-    sys.stdout.write(u"\u001b[1000D") # Move all the way left again
-    sys.stdout.write(u"\u001b[1000D") # Move all the way left again
-    sys.stdout.write(u"\u001b[1000D") # Move all the way left again
-    sys.stdout.write(u"\u001b[1000D") # Move all the way left again
+import os
+
+def core (current_dir) :
+    current_dir_split = current_dir.split("/")
+    del current_dir_split[0]
+    print (current_dir_split)
+    if len(current_dir.split("/")) == 3:
+        return os.environ.get('USER') + "$ "
+    return current_dir + " "
--- default_plugin_core.py
+++ default_plugin_core.py
@@ -1,6 +1,8 @@
-def core () :
-    return ">>> "
-    sys.stdout.write(u"\u001b[1000D") # Move all the way left again
-    sys.stdout.write(u"\u001b[1000D") # Move all the way left again
-    sys.stdout.write(u"\u001b[1000D") # Move all the way left again
-    sys.stdout.write(u"\u001b[1000D") # Move all the way left again
+import os
+
+def core (current_dir) :
+    current_dir_split = current_dir.split("/")
+    del current_dir_split[0]
+    if len(current_dir.split("/")) == 3:
+        return os.environ.get('USER') + "$ "
+    return current_dir + " "
