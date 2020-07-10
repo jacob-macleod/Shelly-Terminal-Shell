8cc533772f82c886d78ad27025380239f78f0c10
jacob-macleod <jacob.e.macleod@gmail.com>
2020-07-10 17:10:37 +0100

    Updated readme.md and cleaned code

--- README.md
+++ README.md
@@ -1,38 +1,26 @@
 # Shelly Terminal Shell
 Version 1
 
-Shelly is a terminal shell written in python
+## Shelly is a terminal shell written in python
 ![Sorry, this image cannot be displayed](https://github.com/jacob-macleod/Shelly-Terminal-Shell/blob/master/Screenshot%202020-07-08%20at%205.53.05%20PM.png "Shelly with the default theme")
-This is [Installation][1] link
+
+## Installation:
+To install Shelly, you can clone the repository: `git clone https://github.com/jacob-macleod/Shelly-Terminal-Shell.git`. To run Shelly, you can cd into the repository with `cd Shelly-Terminal-Shell` and run `python3 main.py`. 
 
 
+## Set as default editor:
+There are many ways to set Shelly as your default editor. One way is to add the following line in your .bashrc file, which should be located in your home directory: `python3 <path_to_shelly_installation>/main.py`. 
+<br>Currently, Shelly only supports being run from the same folder where it is installed, so the easiest way to solve this is to move all your Shelly config files to your home directory, where your .bashrc file is located. This can be done by running `mv Shelly-Terminal-Shell/ $HOME` when in your home directory.
+<br><br>However, this may not be ideal, since you may not want lots of files in your home directory. Therefore, you can keep Shelly in it's own folder by doing the following:
+<br>Replace line 7 in main.py with command_history_location = <path_to_shelly_installation> + "/command_history.txt"
+<br>Replace line 24 in main.py with `with open("<path_to_shelly_installation>/shellyRC.txt") as rc:`
+<br>Replace line 41 in main.py with with open("<path_to_shelly_installation>/core_placeholder.py", "w") as f1:
+<br>Replace line 46 in main.py with with open("<path_to_shelly_installation>/user_placeholder.py", "w") as f1:
+<br>You will need to make sure you have the correct indentation on these lines!
+<br>Replace the file paths to the pluginns in shellyRC.txt with the absolute file paths, and please open an issue if there are any problems with this!
 
+## Contribution:
+We would love any contributors! The issues labeled 'good first issue' are good things to contribute towards.
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-[1]: Is it working?
+## Developing Plugins:
+Shelly can be easily updated through plugins. There are two types of plugins, core and user plugins. Core plugins display the current directory the user is in and the usre, 
--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 # Import modules
-import os, sys, tty, termios
+import os
+import sys
+import tty
+import termios
 
 command_history_location = os.getcwd() + "/" + "command_history.txt"
 
-#Define 2 variables
+# Define 2 variables
 global core_plugin_location
 global user_plugin_location
 
@@ -11,27 +14,29 @@ fd = sys.stdin.fileno()
 old_settings = termios.tcgetattr(fd)
 
 
-#Reads ShellyRC.txt to find file location of core and user plugin
-def find_plugin_location () :
-    #Set 2 variables as global
+# Reads ShellyRC.txt to find file location of core and user plugin
+def find_plugin_location():
+    # Set 2 variables as global
     global core_plugin_location
     global user_plugin_location
 
     with open("shellyRC.txt") as rc:
         for line in rc:
-            #Look for file locations in each line
+            # Look for file locations in each line
             line_arr = line.split('"')
-            # Hash denotes the file location of the core plugin, ! denotes the file location of the user plugin
-            if '#' in line_arr[0] :
+            # Hash denotes the file location of the core plugin
+            #  ! denotes the file location of the user plugin
+            if '#' in line_arr[0]:
                 core_plugin_location = line_arr[1]
-            elif '!' in line_arr[0] :
+            elif '!' in line_arr[0]:
                 user_plugin_location = line_arr[1]
 
 
 find_plugin_location()
 
-#Copyies contents of each plugin file to the appropiate placeholder file
-def write_to_placeholder_files (core_file_location, user_file_location) :
+
+# Copyies contents of each plugin file to the appropiate placeholder file
+def write_to_placeholder_files(core_file_location, user_file_location):
     with open(core_file_location) as f:
         with open("core_placeholder.py", "w") as f1:
             for line in f:
@@ -42,9 +47,12 @@ def write_to_placeholder_files (core_file_location, user_file_location) :
             for line in f:
                 f1.write(line)
 
+
 write_to_placeholder_files(core_plugin_location, user_plugin_location)
 
-#Once the plugin contents are copied to the placeholder files, the appropiate plugins can be imported
+
+# Once the plugin contents are copied to the placeholder files,
+#  the appropiate plugins can be imported
 from core_placeholder import core
 from user_placeholder import user
 
@@ -55,26 +63,26 @@ input = ""
 
 def command_line():
     tty.setraw(sys.stdin)
-    while True: # loop for each line
-    # Define data-model for an input-string with a cursor
+    while True:
+        # Define data-model for an input-string with a cursor
         input = ""
         index = 0
-        while True: # loop for each character
-            char = ord(sys.stdin.read(1)) # read one char and get char code
+        while True:  # loop for each character
+            char = ord(sys.stdin.read(1))  # read one char and get char code
 
             # ELif char == CTR-C
             if char == 3:
                 return
 
             elif 32 <= char <= 126:
-                #Store letter in index
+                # Store letter in index
                 input = input[:index] + chr(char) + input[index:]
                 index += 1
 
             elif char in {10, 13}:
                 sys.stdout.write(u"\u001b[1000D")
-                print ()
-                #Save command to txt file
+                print()
+                # Save command to txt file
                 with open(command_history_location, "a+") as f:
                     f.write(input + "\n")
 
@@ -84,7 +92,6 @@ def command_line():
                 # Changing directories:
                 try:
                     os.chdir(input.split(" ")[1])
-
                 except:
                     pass
 
@@ -99,26 +106,27 @@ def command_line():
             elif char == 27:
                 next1, next2 = ord(sys.stdin.read(1)), ord(sys.stdin.read(1))
                 if next1 == 91:
-                    if next2 == 68: # Left
+                    if next2 == 68:  # Left
                         index = max(0, index - 1)
-                    elif next2 == 67: # Right
+                    elif next2 == 67:  # Right
                         index = min(len(input), index + 1)
 
-            #Elif char == backspace
-            elif char == 127 :
+            # Elif char == backspace
+            elif char == 127:
                 input = input[:index-1] + input[index:]
                 index -= 1
 
             # Print current input-string
-            sys.stdout.write(u"\u001b[1000D") # Move all the way left
+            sys.stdout.write(u"\u001b[1000D")  # Move all the way left
             sys.stdout.write(u"\u001b[2K")    # Clear the line
             output = core() + user(input, command_history_location)
             sys.stdout.write(output)
-            #sys.stdout.write("\u001b[31m" + input + "\u001b[37m" + "string_to_return" + "")
-            sys.stdout.write(u"\u001b[1000C") # Move all the way left again
+            # sys.stdout.write("\u001b[31m" + input + "\u001b[37m" + "string_to_return" + "")
+            sys.stdout.write(u"\u001b[1000C")  # Move all the way left again
 
             if index > 0:
-                sys.stdout.write(u"\u001b[" + str(index) + "C") # Move cursor too index
+                sys.stdout.write(u"\u001b[" + str(index) + "C") 
+            # Move cursor too index
             sys.stdout.flush()
 
 command_line()
