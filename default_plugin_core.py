1329f26b9931bbeb812bea0c18c936ef75898d2a
jacob-macleod <jacob.e.macleod@gmail.com>
2020-07-19 17:42:33 +0100

    Added git support for the default plugin

--- default_plugin_core.py
+++ default_plugin_core.py
@@ -1,3 +1,12 @@
 import subprocess as sp
-def core () :
-    return "\033[31m" + sp.getoutput("whoami") + "\033[0m~" + "\033[34m" +  sp.getoutput("pwd") + "/\033[0m~$ "
+
+# Suggested cursor offset in shellyRC.txt = 23
+def core ():
+    #Include the git branch if possible
+    branch = sp.getoutput("git branch")
+    if "fatal" in branch:
+        branch = ''
+    else: 
+        branch = " git:(" + branch + ")"
+
+    return "\033[31m" + sp.getoutput("whoami") + "\033[0m~" + "\033[34m" +  sp.getoutput("pwd") + "\033[32m" + branch + "/\033[0m~$ "
